fbaf6e02fae62701532ac1ce9ec464e0bf11ef39
Bob Mittmann <bmittmann@mircomgroup.com>
2016-04-29 15:44:15 -0400

    Added keyboard codes descriptions on monitor.c.

--- boot/monitor.c
+++ boot/monitor.c
@@ -84,6 +84,10 @@
 #include <gdb.h>
 #endif
 
+/* ASCII Keyboard codes */
+
+#define _NULL_ 0x00 /* Null (Ctrl+@) */
+#define CTRL_A 0x01 /* SOH */
 #define CTRL_B 0x02 /* STX */
 #define CTRL_C 0x03 /* ETX */
 #define CTRL_D 0x04 /* EOT */
@@ -96,19 +100,24 @@
 #define CTRL_K 0x0b /* VT */
 #define CTRL_L 0x0c /* FF */
 #define CTRL_M 0x0d /* CR */
-#define CTRL_N 0x0e
-#define CTRL_O 0x0f
-#define CTRL_P 0x10
-#define CTRL_Q 0x11
-#define CTRL_R 0x12
-#define CTRL_S 0x13
-#define CTRL_T 0x14
-#define CTRL_U 0x15
-#define CTRL_V 0x16
-#define CTRL_W 0x17
-#define CTRL_X 0x18
-#define CTRL_Y 0x19
-#define CTRL_Z 0x1a
+#define CTRL_N 0x0e /* SO */
+#define CTRL_O 0x0f /* SI */
+#define CTRL_P 0x10 /* DLE */
+#define CTRL_Q 0x11 /* DC1 */
+#define CTRL_R 0x12 /* DC2 */
+#define CTRL_S 0x13 /* DC3 */
+#define CTRL_T 0x14 /* DC4 */
+#define CTRL_U 0x15 /* NAK */
+#define CTRL_V 0x16 /* SYN */
+#define CTRL_W 0x17 /* ETB */
+#define CTRL_X 0x18 /* CAN */
+#define CTRL_Y 0x19 /* EM */
+#define CTRL_Z 0x1a /* SUB */
+#define _ESC_  0x1b /* ESC (Ctrl+[) */
+#define _FS_   0x1c /* FS  (Ctrl+\) */
+#define _GS_   0x1d /* GS  (Ctrl+]) */
+#define _RS_   0x1e /* RS  (Ctrl+^) */
+#define _US_   0x1f /* US  (Ctrl+_) */
 
 extern int __heap_end;
 const void * heap_end = &__heap_end; 
